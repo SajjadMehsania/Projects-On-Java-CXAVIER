a65837c6fb92ea1004681a7bcee653ef21ead3b6
SajjadMehsania <mehsaniasajjad@gmail.com>
2025-01-08 13:55:10 +0530

    Added Number Reverse Triangle program

---
+++ NumberReverseTriangle.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class NumberReverseTriangle {
+    public static void main(String[] args) {
+        System.out.println("Enter number of rows");
+
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=0;i<=n ;i++){
+            for(int j =0 ; j<=i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("--------------------");
+
+        for(int i=n;i>=0 ;i--){
+            for(int j =i ; j>=0; j--){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("--------------------");
+
+        System.out.println("--------------------");
+
+        for(int i=n;i>=0 ;i--){
+            for(int j =i ; j<=n; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
